4c7cf835daa39afe6d62aed075aca2056cfcc008
websosman <contact@websosman.com>
2023-10-02 18:50:25 +0200

    Project Scaffolding

---
+++ bookstore/src/main/java/ch/sosman/bookstore/config/WebConfig.java
@@ -0,0 +1,13 @@
+package ch.sosman.bookstore.config;
+
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
+  @Override
+  public void customize(final ConfigurableServletWebServerFactory factory) {
+    factory.setPort(8080);
+  }
+}
